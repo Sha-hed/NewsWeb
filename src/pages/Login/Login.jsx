77936825d495ea398655ab8730b2e3c7266f7154
Sha-hed <shahedcse14@gmail.com>
2024-04-05 22:53:57 +0600

    Hello World

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h3 className="text-3xl">This is Login Page</h3>
+        </div>
+    );
+};
+
+export default Login;
